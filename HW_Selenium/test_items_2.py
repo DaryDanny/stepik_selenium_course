f1a57dcea0654a0edf87dfdb05400e24f0cabfd4
DaryDanny <danchenko_dasha@mail.ru>
2021-11-29 12:08:40 +0300

    add solution for lesson 3.6.2

--- HW_Selenium/test_items_2.py
+++ HW_Selenium/test_items_2.py
@@ -8,10 +8,14 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 #ссылка, по которой нет кнопки добавления в корзину
 #link = "http://selenium1py.pythonanywhere.com/accounts/login/"
 
-def test_guest_should_see_login_link(browser):
+def test_add_button_existence(browser):
     browser.implicitly_wait(5)
     browser.get(link)
     #добавила sleep только для того, чтобы визуально оценить выбор нужного языка, для ожидания прогрузки кнопки на странице реализован implicitly_wait
     time.sleep(5)
-    elements = browser.find_elements_by_css_selector("button.btn-add-to-basket")
-    assert len(elements) == 1, f"Add to basket button is NOT on the page!"
+    try:
+        browser.find_element_by_css_selector("button.btn-add-to-basket")
+        print("Add to basket button is on the page!")
+
+    except NoSuchElementException:
+        pytest.fail("Add to basket button is NOT on the page!")
